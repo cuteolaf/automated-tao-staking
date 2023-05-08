46d10c37ce782e4ab6ab8ff3f04250ba7f978485
cuteolaf <oliverlim818@gmail.com>
2023-05-08 10:48:36 -0700

    feat: mint tokens to TAO senders

--- proxy-bot/index.js
+++ proxy-bot/index.js
@@ -1,30 +1,79 @@
 require("dotenv").config();
 
+const { ApiPromise, WsProvider, Keyring } = require("@polkadot/api");
+const { ContractPromise } = require("@polkadot/api-contract");
+const { BN, BN_ONE } = require("@polkadot/util");
+
 // Testnet
-const NETWORK_URL =
-  process.env.NETWORK_URL || "wss://test.finney.opentensor.ai:443";
-// const NETWORK_URL = "";
-const PROXY_ADDRESS =
-  process.env.PROXY_ADDRESS ||
-  "5G9Df1fBN89mJcgebcRpvJ2YThaWzEN94YZVptPio1a4SFLT";
+const URL_BITTENSOR_NETWORK =
+  process.env.URL_BITTENSOR_NETWORK || "wss://test.finney.opentensor.ai:443";
+const URL_CONTRACT_NODE =
+  process.env.URL_CONTRACT_NODE || "wss://rpc.shibuya.astar.network";
+const STTAO_CONTACT_ADDRESS =
+  process.env.STTAO_CONTACT_ADDRESS ||
+  "YCLgHfwAS4B1jUbF6UDbTRP29PafGkxBvA1BdbdfEFfx1wy";
 const PROXY_SEED =
   process.env.PROXY_SEED ||
-  "grace enact rigid title broccoli owner access update swarm alpha mail rookie";
-
-const getProxyAccount = () => {
-  return {};
-};
+  "young describe sugar civil bench dizzy salt submit balance trade appear frozen";
 
 const proxyBot = async () => {
-  const { ApiPromise, WsProvider } = require("@polkadot/api");
-
-  console.log("Connecting to network....");
-  const wsProvider = new WsProvider(NETWORK_URL);
-
-  const api = new ApiPromise({ provider: wsProvider });
+  // Connect to Bittensor network
+  console.log("Connecting to Bittensor network....");
+  const api = new ApiPromise({
+    provider: new WsProvider(URL_BITTENSOR_NETWORK),
+  });
   await api.isReady;
-
   console.log("API is ready");
+
+  // Connect to contracts node
+
+  console.log("Connecting to contracts node");
+  const contractApi = new ApiPromise({
+    provider: new WsProvider(URL_CONTRACT_NODE),
+  });
+  await contractApi.isReady;
+  console.log("Contact API is ready");
+
+  const abi = require("./assets/contract.json");
+  const stTao = new ContractPromise(contractApi, abi, STTAO_CONTACT_ADDRESS);
+
+  const keyringBt = new Keyring({
+    ss58Format: api.registry.chainSS58,
+    type: "sr25519",
+  });
+  const keyringCN = new Keyring({
+    ss58Format: contractApi.registry.chainSS58,
+    type: "sr25519",
+  });
+  const proxyOnBt = keyringBt.createFromUri(PROXY_SEED);
+  const proxyOnCN = keyringCN.createFromUri(PROXY_SEED);
+
+  const mintTokens = async (to, amount) => {
+    const MAX_CALL_WEIGHT = new BN(5_000_000_000_000).isub(BN_ONE);
+    const PROOFSIZE = new BN(1_000_000);
+    const { gasRequired, result, output } = await stTao.query.mint(
+      proxyOnCN.address,
+      {
+        gasLimit: contractApi.registry.createType("WeightV2", {
+          refTime: MAX_CALL_WEIGHT,
+          proofSize: PROOFSIZE,
+        }),
+        storageDepositLimit: null,
+      },
+      to,
+      amount
+    );
+    const gasLimit = api.registry.createType("WeightV2", gasRequired);
+    await stTao.tx
+      .mint({ gasLimit, storageDepositLimit: null }, to, amount)
+      .signAndSend(proxyOnCN, async (res) => {
+        if (res.status.isInBlock) {
+          console.log("in a block");
+        } else if (res.status.isFinalized) {
+          console.log("finalized");
+        }
+      });
+  };
 
   // Subscribe to system events via storage
   api.query.system.events((events) => {
@@ -35,7 +84,6 @@ const proxyBot = async () => {
       } event(s)`
     );
     events.forEach((record) => {
-      // extract the phase, event and the event types
       const { event } = record;
       const { section, method, data } = event;
 
@@ -43,8 +91,13 @@ const proxyBot = async () => {
       const from = data[0].toString();
       const to = data[1].toString();
       const amount = data[2].toString();
-      if (to === PROXY_ADDRESS) {
+      if (to === proxyOnBt.address) {
         console.log(`Received ${amount} TAOs from ${from}`);
+        // The proxy bot should mint stTAO tokens to the sender
+        mintTokens(
+          keyringCN.encodeAddress(keyringBt.decodeAddress(from)),
+          amount
+        );
       }
     });
   });
